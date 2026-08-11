b3267e96656953872e23740a62401ccd9546e993
Beltrán Offerrall <69093559+offerrall@users.noreply.github.com>
2026-08-11 20:10:00 +0200

    Bump version to 0.0.6

--- src/pytypehintweb/__init__.py
+++ src/pytypehintweb/__init__.py
@@ -6,7 +6,7 @@ from pytypehintweb.types import COLOR_PATTERN, EMAIL_PATTERN, Color, Email
 
 STATIC = Path(__file__).parent / "static"
 
-__version__ = "0.0.5"
+__version__ = "0.0.6"
 
 __all__ = [
     "plan_of",
